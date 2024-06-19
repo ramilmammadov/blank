ded6c0dc10fcc4fce74ad179db2233d9139d09c8
ramilmammadov <ramil.mammadof@gmail.com>
2024-06-19 15:27:13 +0400

    First commit

---
+++ src/test/java/com/example/MyDemoProject/MyDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyDemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
