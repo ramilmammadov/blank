588ab198187b7485405d72a6c1a515d48295eb01
ramilmammadov <ramil.mammadof@gmail.com>
2024-06-19 18:20:59 +0400

    actuator implemented

---
+++ src/main/java/com/example/MyDemoProject/config/ActuatorSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.MyDemoProject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ActuatorSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/actuator/**").permitAll() // Adjust security settings as needed
+                                .anyRequest().authenticated()
+                )
+                .csrf().disable();
+        return http.build();
+    }
+}
